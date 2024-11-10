47c8cbeb4e127fcc3ce638a5967c4f53a9ce07ec
vigil33t-cmd <66297807+vigil33t-cmd@users.noreply.github.com>
2024-11-11 04:37:11 +0500

    Code cleanup

--- main.py
+++ main.py
@@ -22,29 +22,6 @@ class TicketForm(StatesGroup):
     cabinetName = State()
     description = State()
 
-@dp.message(Command('stats'))
-async def getStats(message: types.Message):
-    user = await getUser(message.from_user.id)
-    # [ticket['TicketText'] for ticket in [await getTicketByID(tickets) for tickets in user['ClosedTickets']]]
-
-    await message.answer('''
-    <b>bold</b>, <strong>bold</strong>
-<i>italic</i>, <em>italic</em>
-<u>underline</u>, <ins>underline</ins>
-<s>strikethrough</s>, <strike>strikethrough</strike>, <del>strikethrough</del>
-<span class="tg-spoiler">spoiler</span>, <tg-spoiler>spoiler</tg-spoiler>
-<b>bold <i>italic bold <s>italic bold strikethrough <span class="tg-spoiler">italic bold strikethrough spoiler</span></s> <u>underline italic bold</u></i> bold</b>
-<a href="http://www.example.com/">inline URL</a>
-<a href="tg://user?id=259134928">inline mention of a user</a>
-<tg-emoji emoji-id="5368324170671202286">👍</tg-emoji>
-<code>inline fixed-width code</code>
-<pre>pre-formatted fixed-width code block</pre>
-<pre><code class="language-python">pre-formatted fixed-width code block written in the Python programming language</code></pre>
-<blockquote>Block quotation started\nBlock quotation continued\nThe last line of the block quotation</blockquote>
-<blockquote expandable>Expandable block quotation started\nExpandable block quotation continued\nExpandable block quotation continued\nHidden by default part of the block quotation started\nExpandable block quotation continued\nThe last line of the block quotation</blockquote>
-    ''', parse_mode="HTML")
-
-
 @dp.message(Command("start"))
 async def startCommand(message: types.Message):
     keyboard = ReplyKeyboardBuilder()
